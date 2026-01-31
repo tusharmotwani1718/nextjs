0c5faa8d160d4ce1c8ed04884dd3fc44b762697b
Tushar Motwani <tusharmotwani89@gmail.com>
2026-01-31 13:37:14 +0000

    add: add todo notes app with shadcn setup

---
+++ 05_notes_app/src/app/page.js
@@ -0,0 +1,54 @@
+import Image from "next/image"; 4
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+export function CardImage() {
+  return (
+    <Card className="relative mx-auto w-full max-w-sm pt-0">
+      <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+      <img
+        src="https://avatar.vercel.sh/shadcn1"
+        alt="Event cover"
+        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+      />
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>Design systems meetup</CardTitle>
+        <CardDescription>
+          A practical talk on component APIs, accessibility, and shipping
+          faster.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Event</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+
+export default function Home() {
+  return (
+
+    <div className="">
+      <main className="p-5 text-blue-700">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
+          Next Notes
+        </h1>
+        <div>
+          <CardImage />
+        </div>
+      </main>
+    </div>
+  );
+}
